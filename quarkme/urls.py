adf34a43ab10aa4da3c78df905a36fc34d2aaf5e
arpit1997 <solankiarpit1997@gmail.com>
2017-03-05 00:01:16 -0800

    refactor: following pep8 code style

--- quarkme/urls.py
+++ quarkme/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^about/?$', views.about),
-	url(r'^contact/?$', views.contact),
-	url(r'^faq/?$', views.faq),
+    url(r'^about/?$', views.about),
+    url(r'^contact/?$', views.contact),
+    url(r'^faq/?$', views.faq),
     url(r'^how-it-works/?$', views.hit),
-	url(r'^careers/?$', views.careers),
+    url(r'^careers/?$', views.careers),
     url(r'^terms/?$', views.terms),
-	url(r'^login/?$', views.login),
+    url(r'^login/?$', views.login),
     url(r'^student-registration/?$', views.streg),
-	url(r'^tutor-registration/?$', views.tereg),
-	url(r'^/?$', views.home),
+    url(r'^tutor-registration/?$', views.tereg),
+    url(r'^/?$', views.home),
 ]
--- quarkme/views.py
+++ quarkme/views.py
@@ -1,32 +1,42 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 def home(request):
-	return render(request, 'html/index.html')
+    return render(request, 'html/index.html')
+
 
 def about(request):
-	return render(request, 'html/about-us.html')
+    return render(request, 'html/about-us.html')
+
 
 def faq(request):
-	return render(request, 'html/faq.html')
+    return render(request, 'html/faq.html')
+
 
 def contact(request):
-	return render(request, 'html/contact.html')
+    return render(request, 'html/contact.html')
+
 
 def careers(request):
-	return render(request, 'html/careers.html')
+    return render(request, 'html/careers.html')
+
 
 def hit(request):
-	return render(request, 'html/how-it-works.html')
+    return render(request, 'html/how-it-works.html')
+
 
 def terms(request):
-	return render(request, 'html/t&c.html')
+    return render(request, 'html/t&c.html')
+
 
 def login(request):
-	return render(request, 'html/login.html')
+    return render(request, 'html/login.html')
+
 
 def streg(request):
-	return render(request, 'html/student-registration.html')
+    return render(request, 'html/student-registration.html')
+
 
 def tereg(request):
-	return render(request, 'html/tutor-registration.html')
+    return render(request, 'html/tutor-registration.html')
